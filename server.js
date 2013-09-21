462f179a630f84fd4259e2ccf67279b7c042f2af
adam <adam@ajiraffe.com>
2013-09-21 17:49:24 -0400

    server

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var host = process.env.HOST, port = process.env.PORT || 5000;
 var app = require('http').createServer(handler)
 	, io = require('socket.io').listen(app);
 
-// var redis = require("redis").createClient(6379, "127.0.0.1");
+//var redis = require("redis").createClient(6379, "127.0.0.1");
 
-// var url = require("url").parse(process.env.OPENREDIS_URL);
-// var redis = require("redis").createClient(url.port, url.hostname);
-// redis.auth(url.auth.split(":")[1]);
+var url = require("url").parse(process.env.OPENREDIS_URL);
+var redis = require("redis").createClient(url.port, url.hostname);
+redis.auth(url.auth.split(":")[1]);
 
-// //Redis Cloud
-// //process.env.REDISCLOUD_URL=redis://rediscloud:9BrvTFM4UKa3c1gU@pub-redis-15994.us-east-1-4.3.ec2.garantiadata.com:15994
-// var redis = require("redis").createClient(15994, "pub-redis-15994.us-east-1-4.3.ec2.garantiadata.com");
-// redis.auth("9BrvTFM4UKa3c1gU");
+//Redis Cloud
+//process.env.REDISCLOUD_URL=redis://rediscloud:9BrvTFM4UKa3c1gU@pub-redis-15994.us-east-1-4.3.ec2.garantiadata.com:15994
+var redis = require("redis").createClient(15994, "pub-redis-15994.us-east-1-4.3.ec2.garantiadata.com");
+redis.auth("9BrvTFM4UKa3c1gU");
 
 // //Redis To Go
 // //process.env.REDISTOGO_URL=redis://redistogo:8dc1ca1ae85ad5a2b8e17c942455b544@dory.redistogo.com:11101/
