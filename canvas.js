e277d2df0242464d2d655b4b54d0d55980e5f630
adam <adam@ajiraffe.com>
2013-09-21 02:02:09 -0400

    add player

--- canvas.js
+++ canvas.js
@@ -2,22 +2,6 @@ function Player(){
 	this.init = function(xpos, ypos){
 		this.x = xpos;
 		this.y = ypos;
-
-		document.addEventListener('keydown', function(event){
-			if (event.keyCode == window.keys.LEFT_ARROW){
-				this.x -= 1;
-			}
-			else if (event.keyCode == window.keys.RIGHT_ARROW){
-				this.x += 1;
-			}
-			if (event.keyCode == window.keys.UP_ARROW){
-				this.y -= 1;
-
-			}
-			else if (event.keyCode == window.keys.DOWN_ARROW){
-				this.y += 1;
-			}
-		});		
 	}
 
 	this.render = function(){
@@ -25,6 +9,14 @@ function Player(){
 		context.arc(this.x,this.y,10,0,2*Math.PI);
 		context.fill();
 		context.closePath();
+	}
+
+	this.moveX = function(x){
+		this.x += x;
+	}
+
+	this.moveY = function(y){
+		this.y += y;
 	}
 
 }
@@ -39,6 +31,21 @@ function Scene(){
 
 		var newPlayer = new Player();
 		newPlayer.init(10,10);
+
+		document.addEventListener('keydown', function(event){
+			if (event.keyCode == window.keys.LEFT_ARROW){
+				newPlayer.moveX(-1);
+			}
+			else if (event.keyCode == window.keys.RIGHT_ARROW){
+				newPlayer.moveX(1);
+			}
+			if (event.keyCode == window.keys.UP_ARROW){
+				newPlayer.moveY(-1);
+			}
+			else if (event.keyCode == window.keys.DOWN_ARROW){
+				newPlayer.moveY(1);
+			}
+		});		
 
 		setInterval(newPlayer.render(), 30);
 
