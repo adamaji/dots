5316a1aa725e022e71b704a9ac99838297849524
adam <adam@ajiraffe.com>
2013-09-21 02:04:51 -0400

    add player

--- canvas.js
+++ canvas.js
@@ -13,6 +13,7 @@ function Player(){
 
 	this.moveX = function(x){
 		this.x += x;
+		alert("move");
 	}
 
 	this.moveY = function(y){
@@ -35,6 +36,7 @@ function Scene(){
 		document.addEventListener('keydown', function(event){
 			if (event.keyCode == window.keys.LEFT_ARROW){
 				newPlayer.moveX(-1);
+				alert("left");
 			}
 			else if (event.keyCode == window.keys.RIGHT_ARROW){
 				newPlayer.moveX(1);
