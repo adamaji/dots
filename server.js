61548e6c610d648c829c0aee2db214048ea6209a
adam <adam@ajiraffe.com>
2013-09-21 17:57:20 -0400

    server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var host = process.env.HOST, port = 5000;
+var host = process.env.HOST, port = process.env.PORT || 8080;
 //var host = "127.0.0.1", port = 8080;
 var app = require('http').createServer(handler)
 	, io = require('socket.io').listen(app);
