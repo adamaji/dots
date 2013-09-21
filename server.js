4502103a734d1ded4b3e5b99967b6185f1592798
Andrew Gerst <gerst20051@gmail.com>
2013-09-21 16:08:41 -0400

    redis

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var host = process.env.HOST, port = 8080;
 var app = require('http').createServer()
-  , io = require('socket.io').listen(app);
- 
-var redis = require("redis");
-client = redis.createClient();
- 
+	, io = require('socket.io').listen(app);
+
+var url = require("url").parse(process.env.OPENREDIS_URL);
+var redis = require("redis").createClient(url.port, url.hostname);
+redis.auth(url.auth.split(":")[1]);
+
 app.listen(port, host);
 
 io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+	io.set("transports", ["xhr-polling"]); 
+	io.set("polling duration", 10); 
 });
 
 function handler(req, res) {
-    res.writeHead(200);
-    res.end("Hello Socket");
+	res.writeHead(200);
+	res.end("Hello Socket");
 }
 
