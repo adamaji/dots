d3448a304a1bbfef462fd6ed627b9bb61e9fe48b
Andrew Gerst <gerst20051@gmail.com>
2013-09-21 16:19:36 -0400

    redis

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var host = process.env.HOST, port = 8080;
 var app = require('http').createServer()
 	, io = require('socket.io').listen(app);
 
-var url = require("url").parse(process.env.OPENREDIS_URL);
-var redis = require("redis").createClient(url.port, url.hostname);
-redis.auth(url.auth.split(":")[1]);
+//var url = require("url").parse(process.env.OPENREDIS_URL);
+//var redis = require("redis").createClient(url.port, url.hostname);
+//redis.auth(url.auth.split(":")[1]);
+
+var redis = require("redis").createClient(15994, "pub-redis-15994.us-east-1-4.3.ec2.garantiadata.com");
+redis.auth("9BrvTFM4UKa3c1gU");
 
 app.listen(port, host);
 
