a345a102be2cd34f61084f55e1fedf5d2691d361
Andrew Gerst <gerst20051@gmail.com>
2013-09-21 15:37:14 -0400

    redis

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var redis = require("redis");
 client = redis.createClient();
  
 app.listen(8080);
- 
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 function handler(req, res) {
     res.writeHead(200);
     res.end("Hello Socket");
