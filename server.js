c2837786843227f7b533bd94bfbf4a2e0db3f6d2
Andrew Gerst <gerst20051@gmail.com>
2013-09-21 17:23:29 -0400

    ignore node_modules

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var host = process.env.HOST, port = 8080;
+//var host = process.env.HOST, port = 8080;
+var host = "127.0.0.1", port = 8080;
 var app = require('http').createServer()
 	, io = require('socket.io').listen(app);
 
