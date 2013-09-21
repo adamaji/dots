d76ae203a4b408526a5ea27113e08abc30c74110
adam <adam@ajiraffe.com>
2013-09-21 01:16:09 -0400

    canvas test

---
+++ canvas.js
@@ -0,0 +1,22 @@
+function Scene(){
+
+	this.init = function(){
+		canvas = document.getElementById("scene");
+		context = canvas.getContext('2d');
+
+		resize();
+
+		context.beginPath();
+		context.arc(10,10,5,0,2*Math.PI);
+		context.fill();
+		context.closePath();
+	}
+
+	resize = function(){
+		width = window.innerWidth;
+		height = window.innerHeight;
+
+		canvas.width = width;
+		canvas.height = height;
+	}
+}
