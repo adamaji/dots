e6be563fe48e4ddc34b3c162ced12880da0151fd
adam <adam@ajiraffe.com>
2013-09-21 01:47:33 -0400

    add key events

--- canvas.js
+++ canvas.js
@@ -6,6 +6,25 @@ function Scene(){
 
 		resize();
 
+		document.addEventListener('keydown', function(event){
+			if (event.keyCode == window.keys.LEFT_ARROW){
+				//left
+				alert("left");
+			}
+			else if (event.keyCode == window.keys.RIGHT_ARROW){
+				//right
+				alert("right");
+			}
+			if (event.keyCode == window.keys.UP_ARROW){
+				//up
+				alert("up");
+			}
+			else if (event.keyCode == window.keys.DOWN_ARROW){
+				//down
+				alert("down");
+			}
+		});
+
 		context.beginPath();
 		context.arc(10,10,10,0,2*Math.PI);
 		context.fill();
