a3159120882eba359b91a6e9b41081d8c356b33e
adam <adam@ajiraffe.com>
2013-09-22 10:30:50 -0400

    add multi

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ function onClientDisconnect() {
 function onNewPlayer(data) {
 	console.log("new p serv");
 	var newPlayer = new Player(data.x, data.y);
-	newPlayer.setID(this.id);
+	//newPlayer.setID(this.id);
+	newPlayer.id = this.id;
 	this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.x, y: newPlayer.y});
 	var i, existingPlayer;
 	for (i = 0; i < players.length; i++) {
